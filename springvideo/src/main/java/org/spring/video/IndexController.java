999944a7a8d11bfcd277b98b852933ba45505dba
stzhang <st.zhang@cn.iisigroup.com>
2016-09-14 21:59:24 +0800

    init

---
+++ springvideo/src/main/java/org/spring/video/IndexController.java
@@ -0,0 +1,75 @@
+package org.spring.video;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.spring.video.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.core.convert.converter.Converter;
+@RestController
+public class IndexController {
+	
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+		 
+		return "Hello Everybody.";
+		
+    }
+	
+	@RequestMapping(value = "/getUser", method = RequestMethod.GET)
+    @ResponseBody
+    public String getUser(@RequestParam String name) {
+		
+		User u = userRepo.findUser(name);
+		if(u != null)
+			return "userName:" +u.getName() + ",age:" + u.getAge();
+		else
+			return "not found user.";
+		
+    }
+	
+	@RequestMapping(value = "/putUser", method = RequestMethod.GET)
+    @ResponseBody
+    public String putUser(@RequestParam String name, @RequestParam String age) {
+		User u = userRepo.findUser(name);
+		if(u == null){
+			u = new User();
+			u.setName(name);
+		}
+		u.setAge(Integer.parseInt(age) );
+		userRepo.save(u);
+		return "modify succesful.";
+		
+    }
+	
+	@RequestMapping(value = "/listUser", method = RequestMethod.GET)
+    @ResponseBody
+    public Object putUser(@RequestParam(required=false, defaultValue="0") int pageNo) {
+		Sort dataSort = new Sort(Direction.ASC, "name");
+		PageRequest pr = new PageRequest(pageNo, 20, dataSort);
+		Page<User> p = userRepo.findAll(pr);
+		Page<Map<String, String>> pmap = p.map(new Converter<User, Map<String, String>>(){
+					public Map<String, String> convert(User source) {
+						Map<String, String>  dmap = new HashMap<String, String>();
+						dmap.put("name", source.getName());
+						dmap.put("age", source.getAge()+"");
+						return dmap;
+					}
+		});
+		
+		return pmap;
+		
+    }
+	
+ 
+}
---
+++ springvideo/src/main/java/org/spring/video/SpringvideoApplication.java
@@ -0,0 +1,16 @@
+package org.spring.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+//@EnableDiscoveryClient
+public class SpringvideoApplication {
+
+	public static void main(String[] args) {
+//		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		SpringApplication.run(SpringvideoApplication.class, args);
+	}
+}
