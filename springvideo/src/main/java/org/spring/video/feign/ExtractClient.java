d9dfaba59768cdbde5037a4cb165b56cd4ba4340
stzhang <st.zhang@cn.iisigroup.com>
2016-09-18 22:24:11 +0800

    add

--- springvideo/src/main/java/org/spring/video/feign/ExtractClient.java
+++ springvideo/src/main/java/org/spring/video/feign/ExtractClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("extract-service")
+@FeignClient("http://extract-service")
 public interface ExtractClient {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/extract")
