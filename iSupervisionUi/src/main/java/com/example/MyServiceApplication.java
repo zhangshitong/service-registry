ad95014df8b91eafeb1167178865f3f7e8093267
zxt841104@126.com <zxt841104@126.com>
2016-09-22 00:46:48 +0800

    modify properties

--- iSupervisionUi/src/main/java/com/example/MyServiceApplication.java
+++ iSupervisionUi/src/main/java/com/example/MyServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 @EnableConfigurationProperties(WebApplicationServiceUrlConfiguration.class)
 public class MyServiceApplication {
 
