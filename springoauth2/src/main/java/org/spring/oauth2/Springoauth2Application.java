a97bab6d1eda345d5e03e1478dd1da88d77e99e0
zxt841104@126.com <zxt841104@126.com>
2016-09-22 02:13:02 +0800

    modify

--- springoauth2/src/main/java/org/spring/oauth2/Springoauth2Application.java
+++ springoauth2/src/main/java/org/spring/oauth2/Springoauth2Application.java
@@ -3,8 +3,10 @@ package org.spring.oauth2;
 import org.spring.oauth2.user.listener.AuthApplicationReadyListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Springoauth2Application {
 	public static void main(String[] args) {
 //		SpringApplication.run(Springoauth2Application.class, args);
